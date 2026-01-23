389db7485c3be8c125b56a28c89bae32ce024078
phorpho <53391100+phorpho@users.noreply.github.com>
2026-01-23 00:30:58 -0500

    visit opal mountain works

--- TWIW/Scripts/Components/player_data.gd
+++ TWIW/Scripts/Components/player_data.gd
@@ -1,8 +1,40 @@
+@tool
 extends Resource
 
 class_name PlayerData
 
-@export var food: int 
-@export var luxuries: int
-@export var weapons: int
-@export var money: int
+signal playerDataChanged
+
+var _food := 0
+var _luxuries := 0
+var _weapons := 0
+var _money := 0
+
+@export_range(0, 999999, 1)
+var food: int:
+	get: return _food
+	set(value):
+		_food = value
+		emit_signal("playerDataChanged")
+
+@export_range(0, 999999, 1)
+var luxuries: int:
+	get: return _luxuries
+	set(value):
+		_luxuries = value
+		emit_signal("playerDataChanged")
+
+@export_range(0, 999999, 1)
+var weapons: int:
+	get: return _weapons
+	set(value):
+		_weapons = value
+		emit_signal("playerDataChanged")
+
+
+@export_range(0, 999999, 1)
+var money: int:
+	get: return _money
+	set(value):
+		_money = value
+		emit_signal("playerDataChanged")
