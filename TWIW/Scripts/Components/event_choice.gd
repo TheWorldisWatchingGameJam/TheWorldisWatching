fba01e9d82c03e74e058c35a9902e8a117434c53
phorpho <53391100+phorpho@users.noreply.github.com>
2026-01-28 01:30:19 -0500

    wilyam choice event works

--- TWIW/Scripts/Components/event_choice.gd
+++ TWIW/Scripts/Components/event_choice.gd
@@ -2,5 +2,4 @@ extends Resource
 class_name EventChoice
 
 @export var choice_text: String          # Text shown on the button
-@export var choice_dialogue: Array[DialogueItem]  # Dialogue after selecting choice
 @export var choice_effects: Array[EventEffect]   # Effects after selecting choice
